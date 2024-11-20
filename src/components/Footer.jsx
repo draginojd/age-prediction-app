8788eda88dd8fdaa1e1c991ae89be13d4d2d62e1
draginojd <arminfazlikhan@gmail.com>
2024-11-20 15:49:31 +0100

    media queries for phones done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
         <footer className="footer">
             <p>&copy; {new Date().getFullYear()} Created by - Armin Fazli Khan | All rights reserved.</p>
             <div className="footer-socials">
-                <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer">Twitter</a>
-                <a href="https://www.linkedin.com/in/armin-fazli-khan-14858a243/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                <a href="https://github.com/draginojd" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <a href="https://www.linkedin.com/in/armin-fazli-khan-14858a243/" target="_blank"> LinkedIn</a>
+                <a href="https://github.com/draginojd" target="_blank" rel="noopener noreferrer">Github</a>
+                <a href="mailto:arminfazlikhan@gmail.com?subject=Hello%20Armin&body=I%20would%20like%20to%20connect%20with%20you" target="_blank" rel="noopener noreferrer">Email</a>
             </div>
         
         </footer>
